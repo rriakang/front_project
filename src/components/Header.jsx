08efdc9c1eaa56146361891792816860f8db75df
rirakang <rirakang@rirakangs-MacBook-Pro.local>
2024-02-22 22:58:16 +0900

    업데이트

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,36 @@ import React from "react";
 
 const Header =() => {
 
-    return <div> Header </div>
+    return (
+        <header id="header" role ="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">portfolio<em>vite</em></a>
+                </div>
+                <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+                    <ul>
+                        <li><a href="#Intro">intro</a></li>
+                        <li><a href="#skill">skill</a></li>
+                        <li><a href="#site">site</a></li>
+                        <li><a href="#port">port</a></li>
+                        <li><a href="#contact">contact</a></li>
+                    </ul>
+                </nav>
+                <div
+                    className="header__nav__mobile"
+                    id ="headerToggle"
+                    aria-controls="primary-menu"
+                    aria-expanded="false"
+                    role="button"
+                    tabIndex="0"
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+
+
+    );
 
 };
 
