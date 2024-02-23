28c3b3f9268a7978a4ddee6c88a6428b87584e94
rirakang <rirakang@rirakangs-MacBook-Pro.local>
2024-02-23 13:17:16 +0900

    헤더작업 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Header =() => {
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro"
+    },
+    {
+        title: "skill",
+        url: "#skill"
+    },
+    {
+        title: "site",
+        url: "#site"
+    },
+    {
+        title: "portfolio",
+        url: "#port"
+    },
+    {
+        title: "contact",
+        url: "#contact"
+    }
+];
+
+const Header = () => {
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
 
     return (
-        <header id="header" role ="banner">
+        <header id="header" role="banner">
             <div className="header__inner">
                 <div className="header__logo">
-                    <a href="/">portfolio<em>vite</em></a>
+                    <a href="/">portfolio<em>react.js</em></a>
                 </div>
-                <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+                <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
                     <ul>
-                        <li><a href="#Intro">intro</a></li>
-                        <li><a href="#skill">skill</a></li>
-                        <li><a href="#site">site</a></li>
-                        <li><a href="#port">port</a></li>
-                        <li><a href="#contact">contact</a></li>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
                 <div
                     className="header__nav__mobile"
-                    id ="headerToggle"
+                    id="headerToggle"
                     aria-controls="primary-menu"
-                    aria-expanded="false"
-                    role="button"
+                    aria-expanded={show ? "true" : "false"}
+                    role="button" 
                     tabIndex="0"
+                    onClick={toggleMenu}
                 >
                     <span></span>
                 </div>
             </div>
         </header>
-
-
     );
-
 };
 
 export default Header;
+
